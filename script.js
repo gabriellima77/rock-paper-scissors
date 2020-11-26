2982171ac5e761b02e5bed0412b906ed9d251572
Gabriel Lima <gabrielpk1@live.com>
2020-11-26 19:03:35 -0300

    fix conditional

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function playRound(player, computer){
     if(player === computer){
         para.textContent = `It's a Tie! ${player} and ${computer} are equal.`; 
     }
-    else if( (player === 'Paper' && computer === 'Rock')  || (player === 'Rock' && computer === 'Scissors')
-            || (player === 'Scissors' && computer === 'Paper')){
+    else if( (player == 'Paper' && computer == 'Rock')  || (player == 'Rock' && computer == 'Scissors')
+            || (player == 'Scissors' && computer == 'Paper')){
         score[0] += 1;
         document.querySelector('#player').textContent = score[0];
         para.textContent = `You Won! ${player} beats ${computer}.`;
